dc9a8ac00aebda5b849df3d872a805c3ec2da2d8
Anishaagupta <anisha8057@gmail.com>
2020-11-04 21:43:34 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Learn Redux by creating simple app
+
+## screenshot
+![Redux app](./src/ss.png)
+
+## Links
+Redux : https://redux.js.org/
+React-Redux : https://react-redux.js.org/ 
+
+### Video Tutorial
+[Part 1](https://youtu.be/fzETB8l4u9A)
+[Part 2](https://youtu.be/fzETB8l4u9A)
+[Part 3](https://youtu.be/o3aGun-pQhQ)
---
+++ src/store/reducers/postReducer.js
@@ -0,0 +1,30 @@
+ const initState = {
+  posts: [
+    {id: 1, title: 'post one', content: 'this is post one'},
+    {id: 2, title: 'post two', content: 'this is post two'},
+  ],
+};
+
+const postReducer = (state = initState, action) => {
+  const {type,payload}=action
+  switch (type) {
+    case "DELETE_POST":
+      const newPost = state.posts.filter((post) => post.id !==payload );
+      return {
+        posts: newPost,
+      };
+    case "EDIT_POST":
+      const editPost = state.posts.map(data=>{if(data.id===payload.id){data.content=payload.content }return data})
+      return {
+        posts: editPost,
+      };
+    case "ADD_POST":
+      return {
+        posts: [action.post, ...state.posts],
+      };
+    default:
+      return state;
+  }
+};
+
+export default postReducer;
